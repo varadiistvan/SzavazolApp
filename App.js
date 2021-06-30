353594b1f00178aaa85cb66a6fa8ff2a170a756f
varadiistvan <varadiista.1@gmail.com>
2021-06-30 16:16:31 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from './screens/Home';
+import Compass from './screens/Compass';
+import How from './screens/How';
+import Quiz from './screens/Quiz';
+import Where from './screens/Where';
+import Who from './screens/Who';
+import Why from './screens/Why';
+
+const Stack = createStackNavigator()
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <StatusBar hidden={true} />
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" options={{title: ""}} component={Home}/>
+          <Stack.Screen name="Compass" options={{title: ""}} component={Compass}/>
+          <Stack.Screen name="How" options={{title: ""}} component={How}/>
+          <Stack.Screen name="Quiz" options={{title: ""}} component={Quiz}/>
+          <Stack.Screen name="Where" options={{title: ""}} component={Where}/>
+          <Stack.Screen name="Who" options={{title: ""}} component={Who}/>
+          <Stack.Screen name="Why" options={{title: ""}} component={Why}/>
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
