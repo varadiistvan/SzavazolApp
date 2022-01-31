db49b56f9d7da2d5c0491d2ec9dc8f4d1b43334e
István Váradi <varadiista.1@gmail.com>
2022-01-31 15:50:07 +0100

    refactor

--- mobile/screens/Compass.js
+++ mobile/screens/Compass.js
@@ -21,7 +21,7 @@ export default class Compass extends React.Component {
             for(var j = 0; j < 4; j++) {
                 var x = Math.round(Math.random()*10) - 5
                 var y = Math.round(Math.random()*10) - 5
-                kérdések[i].válaszok.push({key: j, szöveg: `Vízszintes ${x}, függőleges ${y}`, érték: {x: x, y: y}})
+                kérdések[i].válaszok.push({key: j, szöveg: `Vízszintes ${x}, függőleges ${y}`, érték: {x: x, y: y}, selected: null})
             }
         }
 
@@ -60,8 +60,9 @@ export default class Compass extends React.Component {
                         {this.state.kérdések[this.state.currentQuestion].válaszok.map(item => {
                             return(
                                 <View key={item.key} style={{width: '100%', alignItems: 'center'}}> 
-                                    <Pressable style={{borderColor: 'gray', borderWidth: 1, borderRadius: 20, width: '80%', backgroundColor: this.state.selected == item.key? 'green': 'transparent'}} onPress={() => {
-                                            this.setState({selected: item.key})
+                                    <Pressable style={{borderColor: 'gray', borderWidth: 1, borderRadius: 20, width: '80%', backgroundColor: this.state.kérdések[this.state.currentQuestion].selected == item.key? 'green': 'transparent'}} onPress={() => {
+                                            this.state.kérdések[this.state.currentQuestion].selected = item.key;
+                                            this.setState({})
                                         }}>
                                         <Text style={{padding: 5, textAlign: 'center'}}>
                                             {item.szöveg}
@@ -73,17 +74,21 @@ export default class Compass extends React.Component {
                     </View>
                     
                     <View style={{flex: 2, flexDirection: 'row', justifyContent: 'space-around', width: '100%'}}>
-                        <Pressable style={{display: 'flex', flexDirection: 'row', alignItems: 'center',}} >
+                        <Pressable style={{display: 'flex', flexDirection: 'row', alignItems: 'center', opacity: this.state.currentQuestion == 0? 0.5: 1}} onPress={() => {
+                            if(this.state.currentQuestion !== 0) {
+                                this.setState({currentQuestion: this.state.currentQuestion - 1, coordinates: {x: this.state.coordinates.x - this.state.kérdések[this.state.currentQuestion-1].válaszok[this.state.kérdések[this.state.currentQuestion-1].selected].érték.x, y: this.state.coordinates.y - this.state.kérdések[this.state.currentQuestion-1].válaszok[this.state.kérdések[this.state.currentQuestion-1].selected].érték.y}})
+                            }
+                        }} >
                             <Icon name='arrow-back' />
                             <Text>Vissza</Text>
                         </Pressable>
-                        <Pressable style={{display: 'flex', flexDirection: 'row', alignItems: 'center', opacity: this.state.selected == null? 0.5: 1}} onPress={() => {
-                            if(this.state.selected != null) {
-                                var értékek = this.state.kérdések[this.state.currentQuestion].válaszok[this.state.selected].érték
+                        <Pressable style={{display: 'flex', flexDirection: 'row', alignItems: 'center', opacity: this.state.kérdések[this.state.currentQuestion].selected == null? 0.5: 1}} onPress={() => {
+                            if(this.state.kérdések[this.state.currentQuestion].selected != null) {
+                                var értékek = this.state.kérdések[this.state.currentQuestion].válaszok[this.state.kérdések[this.state.currentQuestion].selected].érték
                                 if(this.state.currentQuestion == this.state.kérdések.length-1) {
-                                    this.setState({finished: true})
+                                    this.setState({finished: true, coordinates: {x: this.state.coordinates.x > 10? 10: this.state.coordinates.x < -10? -10: this.state.coordinates.x, y: this.state.coordinates.y > 10? 10: this.state.coordinates.y < -10? -10: this.state.coordinates.y}})
                                 }
-                                this.setState({coordinates: {x: this.state.coordinates.x + értékek.x > 10? 10: this.state.coordinates.x + értékek.x, y: this.state.coordinates.y + értékek.y > 10? 10: this.state.coordinates.y + értékek.y}, currentQuestion: this.state.currentQuestion + 1, selected: null})
+                                else this.setState({coordinates: {x: this.state.coordinates.x + értékek.x, y: this.state.coordinates.y + értékek.y}, currentQuestion: this.state.currentQuestion + 1, selected: null})
                             }
                             
                         }} >
@@ -95,14 +100,14 @@ export default class Compass extends React.Component {
                     <View style={{flex: 1}}>
                         <ProgressBar style progress={this.state.currentQuestion/this.state.kérdések.length} />
                     </View>
-                    <BottomBar navigation={this.props.navigation} color={'orange'}/>
+                    {/* <BottomBar navigation={this.props.navigation} color={'orange'}/> */}
                 </SafeAreaView>
             )
         }
         else {
             return(
                 <SafeAreaView style={{justifyContent: 'space-evenly', alignItems: 'center', flex: 1, height: '100%'}}>
-                    <View style={{width: '80%', aspectRatio: 1, flex: 1}}>
+                    <View style={{width: '100%', flex: 1}}>
                         <Svg width="100%" height='100%' viewBox="0 0 220 220" >
                             <Rect fill="#F9BBBB" x="10" y="10" width="100" height="100"  />
                             <Rect fill="#C9E5BD" x="10" y="110" width="100" height="100"  />
@@ -121,7 +126,7 @@ export default class Compass extends React.Component {
                                 )
                             })}
                             <Circle  cx={(this.state.coordinates.x*10 + 110).toString()} cy={(this.state.coordinates.y*-10 + 110).toString()} r="5" fill="red" />
-                            <SvgText fill="red" strokeWidth="0.3" stroke="red" fontSize="8" fontFamily="Poppins_400Regular" x={this.state.coordinates.x*10+110} y={this.state.coordinates.y*-10 + 100} textAnchor="middle" >
+                            <SvgText fill="red" strokeWidth="0.3" stroke="red" fontSize="8" fontFamily="Poppins_400Regular" x={this.state.coordinates.x*10+110} y={this.state.coordinates.y>0? this.state.coordinates.y*-10 + 125: this.state.coordinates.y*-10 + 100} textAnchor="middle" >
                                 A Te eredményed
                             </SvgText>
                             {this.state.pártCheck?
