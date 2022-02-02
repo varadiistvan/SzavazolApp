3f23c982b1de91ce4d15aea2ea57fee23729bcd2
varadiistvan <varadiista.1@gmail.com>
2022-02-02 13:44:48 +0100

    pretty lint 🔥

--- mobile/screens/Program.tsx
+++ mobile/screens/Program.tsx
@@ -1,27 +1,25 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React from "react"
+import { StatusBar } from "expo-status-bar"
+import { View, Text, StyleSheet, Button } from "react-native"
 
-import { BottomBar } from './BottomBar';
+import { BottomBar } from "./BottomBar"
 
 interface ProgramProps {
-    navigation: any
+  navigation: any
 }
 
-export const Program = ({
-    navigation
-}: ProgramProps) => {
-
-    
-    return(
-        <View style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center"
-        }}>
-            <StatusBar hidden={true} />
-            <Text> Program </Text>
-            {/* <BottomBar navigation={this.props.navigation} color={'#BA3692'}/> */}
-        </View>
-    )
+export const Program = ({ navigation }: ProgramProps) => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <StatusBar hidden={true} />
+      <Text> Program </Text>
+      {/* <BottomBar navigation={this.props.navigation} color={'#BA3692'}/> */}
+    </View>
+  )
 }
