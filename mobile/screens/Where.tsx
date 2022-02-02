42f1837ed3dd7b7d2459ddd09862dc796b0b0c50
varadiistvan <varadiista.1@gmail.com>
2022-02-02 13:44:48 +0100

    pretty lint 🔥

--- mobile/screens/Where.tsx
+++ mobile/screens/Where.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React from "react"
+import { StatusBar } from "expo-status-bar"
+import { View, Text, StyleSheet, Button } from "react-native"
 
-import { BottomBar } from './BottomBar';
+import { BottomBar } from "./BottomBar"
 
 interface WhereProps {
-    navigation: any
+  navigation: any
 }
 
-export const Where = ({
-    navigation
-}: WhereProps) => {
-    
-    return(
-        <View style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center"
-        }}>
-            <StatusBar hidden={true} />
-            <Text> Where </Text>
-            {/* <BottomBar navigation={this.props.navigation} color={'#BA3692'}/> */}
-        </View>
-    )
+export const Where = ({ navigation }: WhereProps) => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <StatusBar hidden={true} />
+      <Text> Where </Text>
+      {/* <BottomBar navigation={this.props.navigation} color={'#BA3692'}/> */}
+    </View>
+  )
 }
